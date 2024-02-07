c2ba9f8f5c73feb61ca18a508746008a88703fd4
Pedro <111787020+Pedro0974@users.noreply.github.com>
2024-02-07 16:48:03 -0300

    Update test_models.py

--- categories/tests/test_models.py
+++ categories/tests/test_models.py
@@ -1,1 +1,26 @@
 from django.test import TestCase
+from .models import Category
+
+
+class CategoryModelTestCase(TestCase):
+  def setUp(self:
+      self.category = Category.objects.create(naem='Teste Category')
+
+  def test_category_creation(self):
+    """
+    Test if a category is created successfully
+    """
+    self.assertEqual(self.category.name, 'Test Category')
+
+  def test_category_representation(self):
+    """
+    Teste the string representation of the category
+    """
+    self.assertEqual(str(self.category), 'Test Category')
+
+  def test_category_ordering(self):
+    """
+    Test the ordering of categories
+    """
+    another_category = Category.objects.create(name='Another Category')
+    self.assertLess(self.category.name, another_category.name)
